66527f3ffac23c3be8e2014d61809f7e425f022c
Jess2 <jess2.developer@gmail.com>
2021-02-10 16:46:07 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+}
+
+export default Home;
