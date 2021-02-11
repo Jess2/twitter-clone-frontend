3a24ae96fa788496fbdcf17e9a0565b658efce77
Jess2 <jess2.developer@gmail.com>
2021-02-12 01:14:49 +0900

    feat: profile 페이지 추가

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [{ nickname: '저스틴' }, { nickname: '리아' }, { nickname: '진' }];
+  const followingList = [{ nickname: '헨리' }, { nickname: '로라' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로워 목록" data={followerList} />
+        <FollowList header="팔로잉 목록" data={followingList} />
       </AppLayout>
     </>
   );
