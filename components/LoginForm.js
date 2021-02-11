550dc425841228956d31a36b5b6423b4261ba0b8
Jess2 <jess2.developer@gmail.com>
2021-02-12 00:37:46 +0900

    style: loginForm padding 추가

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -8,6 +8,10 @@ const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
 const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
@@ -25,7 +29,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
   }, [id, password]);
 
   return (
-    <Form onFinish={onSubmitForm}>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -46,7 +50,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
         <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
         <Link href="/signup"><a><Button>회원가입</Button></a></Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   );
 }
 
