89df4b490aa55929a13075afa87ababebdcc2668
Jess2 <jess2.developer@gmail.com>
2021-02-11 23:16:13 +0900

    refactor: 리렌더링시 style 객체가 새로 생성되어 다시 그리는 현상을 styled-components로 해결

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -4,6 +4,11 @@ import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
 import UserProfile from "./UserProfile";
 import LoginForm from "./LoginForm";
+import styled from 'styled-components';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,7 +22,7 @@ const AppLayout = ({ children }) => {
           <Link href="/profile"><a>프로필</a></Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: 'middle '}} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/signup"><a>회원가입</a></Link>
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,6 +1,11 @@
 import { useState, useCallback } from "react";
 import { Form, Input, Button } from "antd";
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
 
 const LoginForm = () => {
   const [id, setId] = useState('');
@@ -32,10 +37,10 @@ const LoginForm = () => {
           required
         />
       </div>
-      <div>
+      <ButtonWrapper>
         <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
         <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-      </div>
+      </ButtonWrapper>
     </Form>
   );
 }
