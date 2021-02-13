de6e6319cae54732d5654e8164622e4d3f3a776e
Jess2 <jess2.developer@gmail.com>
2021-02-13 21:21:16 +0900

    refactor: 리듀서 코드 분리

--- reducers/index.js
+++ reducers/index.js
@@ -1,60 +1,24 @@
 import { HYDRATE } from 'next-redux-wrapper';
-
-const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signupData: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  }
-};
-
-// 액션 생성 함수
-export const loginAction = (data) => {
-  return {
-    type: 'LOG_IN',
-    data
-  }
-}
-
-export const logoutAction = () => {
-  return {
-    type: 'LOG_OUT',
-  }
-}
+import { combineReducers } from 'redux';
+import user from './user';
+import post from './post';
 
 // 리듀서 : 이전 상태와 액션으로 다음 상태를 만들어낸다.
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      return {
-        ...state,
-        ...action.payload
-      };
-    case 'LOG_IN':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        }
-      };
-    case 'LOG_OUT':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: false,
-          user: null,
-        }
-      }
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({
+  // For SSR
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return {
+          ...state,
+          ...action.payload
+        };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
---
+++ reducers/user.js
@@ -0,0 +1,41 @@
+export const initialState = {
+  isLoggedIn: false,
+  user: null,
+  signupData: {},
+  loginData: {},
+};
+
+// 액션 생성 함수
+export const loginAction = (data) => {
+  return {
+    type: 'LOG_IN',
+    data
+  }
+}
+
+export const logoutAction = () => {
+  return {
+    type: 'LOG_OUT',
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOG_IN':
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.data,
+      };
+    case 'LOG_OUT':
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
